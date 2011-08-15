9fbd171889f0142f4557d32fe348d67e9316b999
Cristina <cs@nightshiftcreative.com>
2011-08-15 18:08:39 -0400

    html5 module chrome, removed xhtml and rounded

--- html/modules.php
+++ html/modules.php
@@ -6,48 +6,9 @@
 * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-function modChrome_jexhtml( $module, &$params, &$attribs ) {
-	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 3;
-	$headerClass = isset($attribs['header-class']) ? $attribs['header-class'] : 'je-header';
-	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : NULL;
-	$outlineStyle = isset($attribs['outline-style']) ? $attribs['outline-style'] : NULL;
-	
-	if (!empty($module->content)) { 
-		echo '<div class="moduletable'.$params->get('moduleclass_sfx'); if( $moduleClass ) echo ' '.$moduleClass; if( $outlineStyle ) echo ' outline-'.$outlineStyle; echo ' clearfix">';
-			if ($module->showtitle) { 
-				echo '<h'.$headerLevel.' class="'.$headerClass.'">'.$module->title.'</h'.$headerLevel.'>';
-			}
-			echo $module->content .'
-		</div>';
-	}
-}	
-
-function modChrome_jerounded( $module, &$params, &$attribs ) {
-	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 3;
-	$headerClass = isset($attribs['header-class']) ? $attribs['header-class'] : 'je-header';
-	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : NULL;
-	$roundedStyle = isset($attribs['rounded-style']) ? $attribs['rounded-style'] : NULL;
-	
-	if (!empty($module->content)) {
-		echo '<div class="moduletable'.$params->get('moduleclass_sfx'); if( $moduleClass ) echo ' '.$moduleClass; if( $roundedStyle ) echo ' rounded-'.$roundedStyle; echo '">
-			<div>
-				<div>
-					<div>';		 
-					if ($module->showtitle) {
-						echo '<h'.$headerLevel.' class="'.$headerClass.'">'.$module->title.'</h'.$headerLevel.'>';
-					}
-					echo $module->content .'
-					</div>
-				</div>
-			</div>
-		</div>';
-	}
-}
-?>
-
 <?php function modChrome_div( $module, &$params, &$attribs ) {
 	
-	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 3;
+	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 5;
 	$headerClass = isset($attribs['header-class']) ? $attribs['header-class'] : 'je-header';
 	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : null;
 	if (!empty($module->content)){ ?>
@@ -63,7 +24,7 @@ function modChrome_jerounded( $module, &$params, &$attribs ) {
 ?>
 
 <?php function modChrome_aside( $module, &$params, &$attribs ) {
-	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 3;
+	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 4;
 	$headerClass = isset($attribs['header-class']) ? $attribs['header-class'] : 'je-header';
 	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : null;
 	if (!empty($module->content))
@@ -82,7 +43,7 @@ function modChrome_jerounded( $module, &$params, &$attribs ) {
 ?>
 
 <?php function modChrome_figure( $module, &$params, &$attribs ) {
-	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 3;
+	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 4;
 	$headerClass = isset($attribs['header-class']) ? $attribs['header-class'] : 'je-header';
 	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : null;
 	if (!empty($module->content))
@@ -101,7 +62,7 @@ function modChrome_jerounded( $module, &$params, &$attribs ) {
 ?>
 
 <?php function modChrome_footer( $module, &$params, &$attribs ) {
-	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 3;
+	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 5;
 	$headerClass = isset($attribs['header-class']) ? $attribs['header-class'] : 'je-header';
 	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : null;
 	if (!empty($module->content))
