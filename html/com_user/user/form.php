a0209c8ddd35e24e9b67ee57b6a24ad568c52e25
Matt Thomas <matt@betweenbrain.com>
2011-08-31 21:24:21 -0400

    HTML5 Fixing up com_user/user.

--- html/com_user/user/form.php
+++ html/com_user/user/form.php
@@ -5,16 +5,17 @@
 * @copyright	Copyright (C) 2010, 2011 Matt Thomas | Joomla Engineering. All rights reserved.
 * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
 */
+
+// Joomla 1.5 only
+
 ?>
 
-<div class="profile-edit<?php echo $this->escape($this->params->get('pageclass_sfx')) ?>">
+<section class="profile-edit<?php echo $this->escape($this->params->get('pageclass_sfx')) ?>">
 
 	<?php if($this->params->get('show_page_title',1)) : ?>
-	<header>
 		<h2>
 			<?php echo $this->escape($this->params->get('page_title')) ?>
 		</h2>
-	</header>
 	<?php endif; ?>
 	
 	<script type="text/javascript">
@@ -63,4 +64,4 @@
 		<input type="hidden" name="task" value="save">
 		<?php echo JHTML::_( 'form.token' ); ?>
 	</form>
-</div>
+</section>
