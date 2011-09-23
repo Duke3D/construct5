a804100358503a39666dd9850a5409474866fdae
Matt Thomas <matt@betweenbrain.com>
2011-09-23 15:55:14 -0400

    Release pre-flight.

--- elements/present/upgradecheck.php
+++ elements/present/upgradecheck.php
@@ -10,7 +10,7 @@ class JElementUpgradecheck extends JElement {
 		if(!function_exists("curl_init")) return 'cURL is not supported by your server. Please contact your hosting provider to enable this capability.';
 		//If cURL is supported, check the current version available.
 		else 
-				$version = 0.1;
+				$version = 1;
 				$target = 'http://joomlaengineering.com/upgradecheck/construct5';
 				$curl = curl_init();
 				curl_setopt($curl, CURLOPT_URL, $target);
@@ -19,14 +19,14 @@ class JElementUpgradecheck extends JElement {
 				$str = curl_exec($curl);
 				curl_close($curl);
 				
-				$message = '<label style="max-width:100%">You are using Construct5 version '.$version.'. ';
+				$message = '<label style="max-width:100%">You are using Construct5 version 2.5.'.$version.' Beta. ';
 				
 				//If the current version is out of date, notify the user and provide a download link.
 				if ($version < $str)
-					$message = $message . '<a href="http://joomlaengineering.com" target="_blank">Version '.$str.' is now available.</a><br /><a href="http://joomlaengineering.com/construct5-changelog" target="_blank">See what&rsquo;s new</a>.</label>';
+					$message = $message . '<a href="http://joomlaengineering.com" target="_blank">Version 2.5.'.$str.' is now available.</a><br /><a href="http://joomlaengineering.com/construct5-changelog" target="_blank">See what&rsquo;s new</a>.</label>';
 				//If the current version is up to date, notify the user. 	
 				elseif (($version == $str) || ($version > $str))
-					$message = $message . 'There are no updates available at this time.<br /><a href="http://joomlaengineering.com/construct5-changelog" target="_blank">View the change log</a>.';
-				return $message;							
+					$message = $message . 'There are no updates available at this time.<br /><a href="http://joomlaengineering.com/construct5-changelog" target="_blank">View the change log</a>.</label>';
+				return $message;						
 	  }
 }
